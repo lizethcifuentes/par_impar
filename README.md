101feb3a8077829bfe1af2d334302baa1f94f573
JHONATANGARCIAXD <jhonatangarcia@colegioguaneta.edu.co>
2023-02-17 07:46:00 -0500

    se agrega los programas

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # par_impar
 programa verificar si el numero es para o impar
+
+# diagrama de flujo
+
+![diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ par_impar.py
@@ -0,0 +1,10 @@
+x = int(input("digite un numero: "))
+
+r= x%2
+if r == 0:
+    msj="PAR"
+else:
+    msj="IMPAR"
+print("el numero " + str(x) + " es: " + msj)    
+    
+
